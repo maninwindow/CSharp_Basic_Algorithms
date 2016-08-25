8a983f0589aa988d1901a4410f01097a0e1664b3
Alimjan <maninwindow@hotmail.com>
2016-08-25 14:06:13 +0800

    DotNetPractices: Initialize the .NET practice project.

---
+++ DotNetPractices/GradeStatistics.cs
@@ -0,0 +1,15 @@
+﻿namespace DotNetPractices
+{
+    public class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
+    }
+}
---
+++ DotNetPractices/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractices
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Reference Types practice
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(32.3f);
+            book.AddGrade(42);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.LowestGrade);
+            //GradeBook book1 = new GradeBook();
+            GradeBook book1 = book;
+            book1.AddGrade(43);
+        }
+    }
+}
