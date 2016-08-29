3d38fe2578c4bc6aeb83bbf76aa56b99ad1a7ec8
Alimjan <maninwindow@hotmail.com>
2016-08-30 00:11:00 +0800

    Data Type
    Data type practices

--- DotNetPractices/Program.cs
+++ DotNetPractices/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetPractices.DataTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,18 +12,21 @@ namespace DotNetPractices
         private static void Main(string[] args)
         {
             //Reference Types practice
-            GradeBook book = new GradeBook();
-            book.AddGrade(91);
-            book.AddGrade(32.3f);
-            book.AddGrade(42);
+            //GradeBook book = new GradeBook();
+            //book.AddGrade(91);
+            //book.AddGrade(32.3f);
+            //book.AddGrade(42);
 
-            GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.LowestGrade);
-            //GradeBook book1 = new GradeBook();
-            GradeBook book1 = book;
-            book1.AddGrade(43);
+            //GradeStatistics stats = book.ComputeStatistics();
+            //Console.WriteLine(stats.HighestGrade);
+            //Console.WriteLine(stats.AverageGrade);
+            //Console.WriteLine(stats.LowestGrade);
+            ////GradeBook book1 = new GradeBook();
+            //GradeBook book1 = book;
+            //book1.AddGrade(43);
+
+            //DisplayTriangle.Display();
+            TestPassword.PasswordTest();
         }
     }
 }
