ae5a9c29d74936c4af0d35a77d325d87ce9c86d2
Alimjan <maninwindow@hotmail.com>
2016-08-30 10:07:53 +0800

    DotNetPractices: Pluralsight practice.

--- DotNetPractices/Program.cs
+++ DotNetPractices/Program.cs
@@ -8,21 +8,69 @@ namespace DotNetPractices
 {
     public class Program
     {
+        private delegate int del(int i);
+
         private static void Main(string[] args)
         {
-            //Reference Types practice
-            GradeBook book = new GradeBook();
-            book.AddGrade(91);
-            book.AddGrade(32.3f);
-            book.AddGrade(42);
+            ////Reference Types practice
+            //GradeBook book = new GradeBook();
+            //book.AddGrade(91);
+            //book.AddGrade(32.3f);
+            //book.AddGrade(42);
 
-            GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.LowestGrade);
-            //GradeBook book1 = new GradeBook();
-            GradeBook book1 = book;
-            book1.AddGrade(43);
+            //GradeStatistics stats = book.ComputeStatistics();
+            //Console.WriteLine(stats.HighestGrade);
+            //Console.WriteLine(stats.AverageGrade);
+            //Console.WriteLine(stats.LowestGrade);
+            ////GradeBook book1 = new GradeBook();
+            //GradeBook book1 = book;
+            //book1.AddGrade(43);
+
+            //Array Covariance
+            //object[] objArr = new object[3];
+            string[] daysOfWeek = {
+                                    "Monday",
+                                    "Tuesday",
+                                    "Wednesday",
+                                    "Thersday",
+                                    "Friday",
+                                    "Saturday",
+                                    "Sunday"
+                                };
+            //int indexOfW = Array.FindIndex(daysOfWeek, x => x[0] == 'W');
+            //Console.WriteLine(indexOfW);
+
+            //string[] allWithChars = Array.FindAll(
+            //    daysOfWeek, x => x.Length == 9);
+            //foreach (string item in allWithChars)
+            //    Console.WriteLine(item);
+
+            //object[] objArr2 = daysOfWeek;
+
+            //foreach (object obj in objArr2)
+            //    Console.WriteLine(obj);
+
+            //What Arrays can do?
+            //int[] squares = { 1, 4, 9, 16 };
+
+            //int[] copy = new int[8];
+            //squares.CopyTo(copy, 0);
+            //foreach (int value in copy)
+            //{
+            //    Console.WriteLine(value);
+            //}
+            //Console.WriteLine(string.Format("squares == copy?{0}", squares == copy));
+
+            //Lambda expression examplds
+            //del myDelegate = y => y * y;
+            //int j = myDelegate(5);
+            //Console.WriteLine(j);
+            //Console.ReadLine();
+
+            //Binary search
+            Array.Sort(daysOfWeek);
+            int indexOfSun = Array.BinarySearch(daysOfWeek, "Sunday");
+            Console.WriteLine("Index is:" + indexOfSun);
         }
     }
 }
