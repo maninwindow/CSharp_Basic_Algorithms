56e69a57565fa18f4676e3602361ace66b0ef00b
Alimjan <maninwindow@hotmail.com>
2016-09-30 00:31:35 +0800

    Added function folder again and created new function practice methods.

---
+++ DotNetPractices/Function/CalculateIndividualDigitsOfNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractices.Function
+{
+    public class CalculateIndividualDigitsOfNumber
+    {
+        public static int SumCal(int n)
+        {
+            string n1 = Convert.ToString(n);
+            int sum = 0;
+            for (int i = 0; i < n1.Length; i++)
+                sum += Convert.ToInt32(n1.Substring(i, 1));
+            return sum;
+        }
+    }
+}
---
+++ DotNetPractices/Function/FeibonacciSeries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractices.Function
+{
+    public class FeibonacciSeries
+    {
+        public static int Fibo(int nno)
+        {
+            int num1 = 0;
+            int num2 = 1;
+
+            for (int i = 0; i < nno; i++)
+            {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp + num2;
+            }
+            return num1;
+        }
+    }
+}
