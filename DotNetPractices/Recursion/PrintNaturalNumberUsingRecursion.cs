c0c63e95bd652e62fceaed56a43c7f8071f12d8f
Alimjan <maninwindow@hotmail.com>
2016-10-08 16:33:10 +0800

    DotNetPractices: Began to practice on Recursion methods.

---
+++ DotNetPractices/Recursion/PrintNaturalNumberUsingRecursion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractices.Recursion
+{
+    public class PrintNaturalNumberUsingRecursion
+    {
+        public static int printNatural(int stval, int ctr)
+        {
+            if (ctr < 1)
+            {
+                return stval;
+            }
+            ctr--;
+            Console.Write(" {0} ", stval);
+            return printNatural(stval + 1, ctr);
+        }
+    }
+}
