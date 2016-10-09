c12e744a93f929d015275b5ea904057a0da7a5a3
Alimjan <maninwindow@hotmail.com>
2016-10-09 23:24:19 +0800

    Began to practice C# LINQ and added the first method.

---
+++ DotNetPractices/LINQ/BasicStructureOfLINQ.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractices.LINQ
+{
+    public class LinqExercise1
+    {
+        static void Main()
+        {
+            //  The first part is Data source.  
+            int[] n1 = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.Write("\nBasic structure of LINQ : ");
+            Console.Write("\n---------------------------");
+
+            // The second part is Query creation.  
+            // nQuery is an IEnumerable<int>  
+            var nQuery =
+                from VrNum in n1
+                where (VrNum % 2) == 0
+                select VrNum;
+
+            // The third part is Query execution.  
+
+            Console.Write("\nThe numbers which produce the remainder 0 after divided by 2 are : \n");
+            foreach (int VrNum in nQuery)
+            {
+                Console.Write("{0} ", VrNum);
+            }
+            Console.Write("\n\n");
+        }
+    }
+}
