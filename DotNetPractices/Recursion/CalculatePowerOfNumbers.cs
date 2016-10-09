0750c20e66341b947af7183e5fc6c53cf4368ca0
Alimjan <maninwindow@hotmail.com>
2016-10-09 23:16:23 +0800

    Create CalculatePowerOfNumbers.cs

---
+++ DotNetPractices/Recursion/CalculatePowerOfNumbers.cs
@@ -0,0 +1,30 @@
+using System;  
+public class RecExercise15  
+{  
+   
+ public static void Main()  
+    {  
+    int bNum,pwr;  
+    int result;  
+    Console.Write("\n\n Recursion : Calculate power of any number :\n");  
+    Console.Write("------------------------------------------------\n");      
+       
+    Console.Write(" Input the base  value : ");  
+    bNum = Convert.ToInt32(Console.ReadLine());  
+       
+    Console.Write(" Input the exponent : ");  
+    pwr = Convert.ToInt32(Console.ReadLine());  
+       
+    result=CalcuOfPower(bNum,pwr);//called the function CalcuOfPower  
+       
+    Console.Write(" The value of {0} to the power of {1} is : {2} \n\n",bNum,pwr,result);  
+    }  
+  
+public static int CalcuOfPower( int x,int y)  
+ {  
+  if (y == 0)  
+   return 1;  
+  else  
+   return x * CalcuOfPower( x, y - 1 );  
+ }  
+}
