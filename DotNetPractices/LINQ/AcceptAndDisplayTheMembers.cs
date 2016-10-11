00f21554b5ebf65f820fc7b8daa846ab48f6c4eb
Alimjan <maninwindow@hotmail.com>
2016-10-11 23:33:59 +0800

    DotNetPractices: Added three more methods that gives examples of different scenarios.

---
+++ DotNetPractices/LINQ/AcceptAndDisplayTheMembers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractices.LINQ
+{
+    public class AcceptAndDisplayTheMembers
+    {
+        public static void TestMethod()
+        {
+            int i = 0;
+            int memlist, n, m;
+            List<int> templist = new List<int>();
+            Console.Write("\nLINQ : Accept the members of a list and display the members more than a specific value : ");
+            Console.Write("\n----------------------------------------------------------------------------------------\n");
+
+            Console.Write("Input the number of members in the List : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Member {0} : ", i);
+                memlist = Convert.ToInt32(Console.ReadLine());
+                templist.Add(memlist);
+            }
+
+            Console.Write("\nInput the value above you want to display the members of the List : ");
+            m = Convert.ToInt32(Console.ReadLine());
+
+            List<int> FilterList = templist.FindAll(x => x > m ? true : false);
+            Console.WriteLine("\nThe numbers greater than {0} are : ", m);
+            foreach (var num in FilterList)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DotNetPractices/LINQ/FindStringStartsAndEndsWithSpecificCharacter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractices.LINQ
+{
+    public class FindStringStartsAndEndsWithSpecificCharacter
+    {
+        public static void TestMethod()
+        {
+            string chst, chen;
+            char ch;
+            string[] cities =
+            {
+                "ROME","LONDON","NAIROBI","CALIFORNIA","ZURICH","NEW DELHI","AMSTERDAM","ABU DHABI", "PARIS"
+            };
+
+            Console.Write("\nLINQ : Find the string which starts and ends with a specific character : ");
+            Console.Write("\n-----------------------------------------------------------------------\n");
+            Console.Write("\nThe cities are : 'ROME','LONDON','NAIROBI','CALIFORNIA','ZURICH','NEW DELHI','AMSTERDAM','ABU DHABI','PARIS' \n");
+
+            Console.Write("\nInput starting character for the string : ");
+            ch = (char)Console.Read();
+            chst = ch.ToString();
+            Console.Write("\nInput ending character for the string : ");
+            ch = (char)Console.Read();
+            chen = ch.ToString();
+
+
+            var _result = from x in cities
+                          where x.StartsWith(chst)
+                          where x.EndsWith(chen)
+                          select x;
+            Console.Write("\n\n");
+            foreach (var city in _result)
+            {
+                Console.Write("The city starting with {0} and ending with {1} is : {2} \n", chst, chen, city);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
