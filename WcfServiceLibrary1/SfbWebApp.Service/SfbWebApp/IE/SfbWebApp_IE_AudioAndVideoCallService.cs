5c6a9ecdc36880805f5482435f703766bd2c347d
Alimjan <maninwindow@hotmail.com>
2016-11-16 19:36:26 +0800

    Added new Interface practice

---
+++ WcfServiceLibrary1/SfbWebApp.Service/SfbWebApp/IE/SfbWebApp_IE_AudioAndVideoCallService.cs
@@ -0,0 +1,13 @@
+﻿using SfbWebApp.Core.Interfaces.SfbWebApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfbWebApp.Service.SfbWebApp.Chrome
+{
+    public class SfbWebApp_IE_AudioAndVideoCallService : IAudioAndVideoCall
+    {
+    }
+}
