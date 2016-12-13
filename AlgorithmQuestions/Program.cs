71c993dbb126c28f0a3146e7c048ca7fa8578a1b
Alimjan <maninwindow@hotmail.com>
2016-12-13 18:53:52 +0800

    added new interview questions and solutions.

---
+++ AlgorithmQuestions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmQuestions
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Reverse Integer
+            //int data = -3243254;
+            //Array.ReverseInteger_Solution01(data);
+            //Array.ReverseInteger_Solution02(data);
+
+            //Check duplicates and return the lengs of Array elements.
+            int[] nums = { 2, 2, 3, 4, 5, 5, 4, 8 };
+            //Array.RemoveDuplicates_Solution01(nums);
+            Array.RemoveDuplicates_Solution02(nums);
+        }
+    }
+}
